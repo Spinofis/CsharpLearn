75dd11c3368ffcfcd8747b863badb3aaa17a2a2d
Bartłomiej Piepióra <bartlomiej.piepiora@bakk.com>
2021-07-16 19:37:56 +0200

    init

---
+++ CsharpLearn/Delegates.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CsharpLearn
+{
+    public class Delegates
+    {
+        public void CallTestActionParam() 
+        {
+            DateTime now = DateTime.Now;
+            TestActionParam((now) => 
+            {
+                Console.WriteLine("Now as parametr");
+                Console.WriteLine(now.AddDays(1)); 
+            });
+            TestActionParam((d) => 
+            {
+                Console.WriteLine("None as parametr");
+                Console.WriteLine(d.AddDays(1)); 
+            });
+        }
+
+        public void CallTestActionParametrless()
+        {
+            DateTime now = DateTime.Now;
+            TestActionParametrless(() =>
+            {
+                Console.WriteLine("Now as parametr");
+                Console.WriteLine(DateTime.Now.AddDays(1));
+            });
+            TestActionParametrless(() =>
+            {
+                Console.WriteLine("None as parametr");
+                Console.WriteLine(DateTime.MinValue.AddDays(1));
+            });
+        }
+
+        private void TestActionParam(Action<DateTime> funcDate) 
+        {
+            funcDate.Invoke(new DateTime(1995,6,25));
+        }
+
+        private void TestActionParametrless(Action funcDate)
+        {
+            funcDate.Invoke();
+        }
+    }
+}
---
+++ CsharpLearn/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CsharpLearn
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Delegates delegates = new Delegates();
+            delegates.CallTestActionParametrless();
+        }
+    }
+}
